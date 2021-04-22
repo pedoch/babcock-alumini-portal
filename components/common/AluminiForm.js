b8c4422d227618d4ae7f791e1ef523cd982b2f83
Pedro Ogheneochuko <pedroogheneochuko@gmail.com>
2021-04-22 05:10:33 +0100

    Implement other API

---
+++ components/common/AluminiForm.js
@@ -0,0 +1,261 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function AluminiForm({ alumini, cb, loading, setLoading }) {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [course, setCourse] = useState("");
+  const [gradYear, setGradYear] = useState("");
+  const [admissionYear, setAdmissionYear] = useState("");
+  const [matricNumber, setMatricNumber] = useState("");
+  const [cgpa, setCGPA] = useState("");
+  const [interests, setInterests] = useState("");
+  const [bestLecturer, setBestLecturer] = useState("");
+  const [image, setImage] = useState("");
+  const [imageBlob, setImageBlob] = useState(null);
+  const [dates, setDates] = useState([]);
+  const [beforeDates, setBeforeDates] = useState([]);
+
+  let todayDate = new Date();
+  useEffect(() => {
+    let years = [];
+    for (let i = todayDate.getFullYear(); i >= 1959; i--) {
+      years.push(i);
+    }
+
+    setDates(years);
+
+    years = [];
+    for (let i = todayDate.getFullYear() - 4; i >= 1959; i--) {
+      years.push(i);
+    }
+
+    setBeforeDates(years);
+
+    if (alumini) {
+      setFirstname(alumini.firstname);
+      setLastname(alumini.lastname);
+      setEmail(alumini.email);
+      setPhone(alumini.phone);
+      setCourse(alumini.course);
+      setGradYear(alumini.gradYear);
+      setAdmissionYear(alumini.admissionYear);
+      setMatricNumber(alumini.matricNumber);
+      setCGPA(alumini.cgpa);
+      setInterests(alumini.interest);
+      setBestLecturer(alumini.bestLecturer);
+      setImage(alumini.image);
+    }
+  }, []);
+
+  async function submitForm() {
+    try {
+      setLoading(true);
+      const CLOUDINARY_UPLOAD_PRESET = "ayuedatm";
+
+      let data;
+
+      if (imageBlob) {
+        let form = new FormData();
+        form.append("file", imageBlob);
+        form.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+        form.append("folder", "/babcock-alumini/");
+
+        let res = await axios({
+          url: "https://api.cloudinary.com/v1_1/dsbogvjcc/upload/",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          data: form,
+        });
+
+        data = res.data;
+      }
+
+      cb({
+        firstname,
+        lastname,
+        email,
+        phone: parseInt(phone),
+        course,
+        admissionYear: parseInt(admissionYear),
+        gradYear: parseInt(gradYear),
+        matricNumber,
+        cgpa: parseFloat(cgpa),
+        interest: interests,
+        bestLecturer,
+        image: data
+          ? data.secure_url
+          : image
+          ? image
+          : "https://res.cloudinary.com/dsbogvjcc/image/upload/v1619052171/babcock-alumini/user-profile_mphtde.jpg",
+      });
+    } catch (error) {
+      if (!error.response) {
+        toaster.danger("Unable to create alumini", {
+          description: "May be a network error",
+        });
+      } else if (error.response.status === 500) {
+        toaster.danger("Unable to create alumini", {
+          description: "May be a problem from our side. We'll investigate",
+        });
+      }
+    }
+  }
+
+  return (
+    <>
+      <img
+        src={
+          image ||
+          "https://res.cloudinary.com/dsbogvjcc/image/upload/v1619052171/babcock-alumini/user-profile_mphtde.jpg"
+        }
+        className="w-32 h-32 rounded-full mb-5"
+      />
+      <input
+        className="p-2  bg-black border border-yellow-400 text-yellow-400"
+        type="file"
+        onChange={(e) => {
+          setImageBlob(e.target.files[0]);
+          if (e.target.files[0]) {
+            let url = URL.createObjectURL(e.target.files[0]);
+            setImage(url);
+          } else {
+            setImage(
+              "https://res.cloudinary.com/dsbogvjcc/image/upload/v1619052171/babcock-alumini/user-profile_mphtde.jpg"
+            );
+          }
+        }}
+      />
+      <p className="mb-4">Upload Profile Picture</p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          submitForm();
+        }}
+      >
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Firstname"
+          required
+          value={firstname}
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Lastname"
+          required
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+        />
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Phone"
+          required
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <select
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Course"
+          required
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+        >
+          <option>Select Course</option>
+          <option value="Computer Science">Computer Science</option>
+          <option value="Computer Information Systems">
+            Computer Information Systems
+          </option>
+          <option value="Computer Technology">Computer Technology</option>
+          <option value="Software Engineering">Software Engineering</option>
+        </select>
+        <p className=" text-yellow-400">Admission Year</p>
+        <select
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Admission Year"
+          required
+          value={admissionYear}
+          onChange={(e) => setAdmissionYear(e.target.value)}
+        >
+          <option>Select Year</option>
+          {beforeDates ? (
+            beforeDates.map((date) => (
+              <option key={date} value={date}>
+                {date}
+              </option>
+            ))
+          ) : (
+            <option>No Dates Available</option>
+          )}
+        </select>
+        <p className=" text-yellow-400">Graduation Year</p>
+        <select
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Graduation Year"
+          required
+          value={gradYear}
+          onChange={(e) => setGradYear(e.target.value)}
+        >
+          <option>Select Year</option>
+          {dates ? (
+            dates.map((date) => (
+              <option key={date} value={date}>
+                {date}
+              </option>
+            ))
+          ) : (
+            <option>No Dates Available</option>
+          )}
+        </select>
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Matric Number"
+          required
+          value={matricNumber}
+          onChange={(e) => setMatricNumber(e.target.value)}
+        />
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="CGPA"
+          required
+          value={cgpa}
+          onChange={(e) => setCGPA(e.target.value)}
+        />
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Interest: interests"
+          required
+          value={interests}
+          onChange={(e) => setInterests(e.target.value)}
+        />
+        <input
+          className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+          placeholder="Best Lecturer"
+          required
+          value={bestLecturer}
+          onChange={(e) => setBestLecturer(e.target.value)}
+        />
+        <button
+          className="p-2 w-full bg-yellow-400 border border-black text-black"
+          disabled={loading}
+          type="submit"
+        >
+          {loading ? "Submitting" : "Submit"}
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default AluminiForm;
--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,30 @@ import {
   toaster,
 } from "evergreen-ui";
 import axios from "axios";
+import AluminiForm from "../components/common/AluminiForm";
+import { SP } from "next/dist/next-server/lib/utils";
 
 export default function Home() {
   const [dates, setDates] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [showNewModal, setShowNewModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showSearchModal, setShowSearchModal] = useState(false);
   const [selectedAlumini, setSelectedAlumini] = useState(null);
   const [alumini, setAlumini] = useState([]);
   const [aluminiLoading, setAluminiLoading] = useState(false);
+  const [creatingAlumini, setCreatingAlumini] = useState(false);
+  const [editingAlumini, setEditingAlumini] = useState(false);
+  const [selectedYear, setSelectedYear] = useState(null);
+  const [stage, setStage] = useState(1);
+  const [foundAlumini, setFoundAlumini] = useState({});
+  const [matricNumber, setMatrictNumber] = useState(null);
+  const [matError, setMatError] = useState(false);
+  const [useMatNum, setUseMatNum] = useState(false);
+  const [searchingAlumini, setSearchingAlumini] = useState(false);
+  const [search, setSearch] = useState("");
+  const [searchError, setSearchError] = useState(false);
+  const [foundSearch, setFoundSearch] = useState({});
 
   let todayDate = new Date();
   useEffect(() => {
@@ -39,7 +56,9 @@ export default function Home() {
     setAluminiLoading(true);
 
     try {
-      let { data } = await axios.get(`/api/alumini?year=${year}`);
+      let { data } = await axios.get(
+        `/api/alumini?year=${year || selectedYear || todayDate.getFullYear()}`
+      );
 
       setAlumini(data.alumini);
     } catch (error) {
@@ -56,6 +75,54 @@ export default function Home() {
       setAluminiLoading(false);
     }
   }
+
+  async function createAlumini(payload) {
+    setCreatingAlumini(true);
+
+    try {
+      let { data } = await axios.post(`/api/alumini`, payload);
+
+      toaster.success(data.message);
+      fetchAlumini();
+    } catch (error) {
+      if (!error.response) {
+        toaster.danger("Unable to create alumini", {
+          description: "May be a network error",
+        });
+      } else if (error.response.status === 500) {
+        toaster.danger("Unable to create alumini", {
+          description: "May be a problem from our side. We'll investigate",
+        });
+      }
+    } finally {
+      setCreatingAlumini(false);
+    }
+  }
+
+  async function updateAlumini(payload) {
+    setEditingAlumini(true);
+
+    try {
+      let { data } = await axios.patch(`/api/alumini`, payload);
+
+      toaster.success(data.message);
+      fetchAlumini();
+    } catch (error) {
+      if (!error.response) {
+        toaster.danger("Unable to update alumini", {
+          description: "May be a network error",
+        });
+      } else if (error.response.status === 500) {
+        toaster.danger("Unable to update alumini", {
+          description: "May be a problem from our side. We'll investigate",
+        });
+      }
+    } finally {
+      setEditingAlumini(false);
+      setShowEditModal(false);
+      setStage(1);
+    }
+  }
   return (
     <div
       className="w-full h-screen bg-cover bg-no-repeat bg-center bg-fixed"
@@ -69,8 +136,22 @@ export default function Home() {
           Babcock Alumini Portal
         </p>
         <div className="flex space-x-5 phone:hidden">
-          <button className="p-2 bg-yellow-400 text-black">New Alumini</button>
-          <button className="p-2 bg-yellow-400 text-black">
+          <button
+            className="p-2 bg-yellow-400 text-black"
+            onClick={() => setShowSearchModal(true)}
+          >
+            Search Alumini
+          </button>
+          <button
+            className="p-2 bg-yellow-400 text-black"
+            onClick={() => setShowNewModal(true)}
+          >
+            New Alumini
+          </button>
+          <button
+            className="p-2 bg-yellow-400 text-black"
+            onClick={() => setShowEditModal(true)}
+          >
             Update Alumini
           </button>
         </div>
@@ -82,12 +163,26 @@ export default function Home() {
             <Menu>
               <Menu.Group>
                 <Menu.Item>
-                  <button className="p-2 w-full bg-black text-yellow-400 mb-5">
+                  <button
+                    className="p-2 w-full bg-black text-yellow-400 mb-5"
+                    onClick={() => setShowSearchModal(true)}
+                  >
+                    Search Alumini
+                  </button>
+                </Menu.Item>
+                <Menu.Item>
+                  <button
+                    className="p-2 w-full bg-black text-yellow-400 mb-5"
+                    onClick={() => setShowNewModal(true)}
+                  >
                     New Alumini
                   </button>
                 </Menu.Item>
                 <Menu.Item>
-                  <button className="p-2 w-full bg-black text-yellow-400">
+                  <button
+                    className="p-2 w-full bg-black text-yellow-400"
+                    onClick={() => setShowEditModal(true)}
+                  >
                     Update Alumini
                   </button>
                 </Menu.Item>
@@ -106,10 +201,15 @@ export default function Home() {
           defaultValue={todayDate.getFullYear()}
           onChange={(e) => {
             fetchAlumini(e.target.value);
+            setSelectedYear(e.target.value);
           }}
         >
           {dates ? (
-            dates.map((date) => <option value={date}>{date}</option>)
+            dates.map((date) => (
+              <option key={date} value={date}>
+                {date}
+              </option>
+            ))
           ) : (
             <option>No Dates Available</option>
           )}
@@ -135,24 +235,23 @@ export default function Home() {
         isShown={showModal}
         title="View Alumini"
         hasFooter={false}
-        hasHeader={false}
+        // hasHeader={false}
         shouldCloseOnOverlayClick={true}
         containerProps={{ backgroundColor: "black" }}
-        topOffset="18%"
         preventBodyScrolling
         onCloseComplete={() => {
           setShowModal(false);
         }}
       >
-        <div className="rounded flex flex-col items-center p-6 bg-black text-white">
-          <span
+        <div className="rounded flex flex-col items-center p-6 bg-black text-white phone:p-0">
+          {/* <span
             className="absolute right-4 top-1 text-3xl cursor-pointer"
             onClick={() => {
               setShowModal(false);
             }}
           >
             &times;
-          </span>
+          </span> */}
           <img
             src={selectedAlumini?.image}
             className="w-32 h-32 rounded-full mb-5"
@@ -185,6 +284,237 @@ export default function Home() {
               {selectedAlumini?.bestLecturer}
             </p>
           </div>
+        </div>
+      </Dialog>
+      <Dialog
+        isShown={showNewModal}
+        title="New Alumini"
+        hasFooter={false}
+        // hasHeader={false}
+        shouldCloseOnOverlayClick={true}
+        containerProps={{ backgroundColor: "black" }}
+        preventBodyScrolling
+        onCloseComplete={() => {
+          setShowNewModal(false);
+        }}
+      >
+        <div
+          id="alumini_form"
+          className="rounded flex flex-col items-center p-6 bg-black text-white phone:p-0"
+        >
+          {/* <span
+            className="absolute right-4 top-1 text-3xl cursor-pointer"
+            onClick={() => {
+              setShowNewModal(false);
+            }}
+          >
+            &times;
+          </span> */}
+          <AluminiForm
+            cb={createAlumini}
+            loading={creatingAlumini}
+            setLoading={setCreatingAlumini}
+          />
+        </div>
+      </Dialog>
+      <Dialog
+        isShown={showEditModal}
+        title="Update Alumini"
+        hasFooter={false}
+        // hasHeader={false}
+        shouldCloseOnOverlayClick={true}
+        containerProps={{ backgroundColor: "black" }}
+        preventBodyScrolling
+        onCloseComplete={() => {
+          setShowEditModal(false);
+          setStage(1);
+        }}
+      >
+        <div
+          id="alumini_form"
+          className="rounded flex flex-col items-center p-6 bg-black text-white phone:p-0"
+        >
+          {/* <span
+            className="absolute right-4 top-1 text-3xl cursor-pointer"
+            onClick={() => {
+              setShowEditModal(false);
+              setStage(1);
+            }}
+          >
+            &times;
+          </span> */}
+          {stage === 1 ? (
+            <div>
+              <input
+                className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+                placeholder="Matric Number"
+                required
+                value={matricNumber}
+                onChange={(e) => setMatrictNumber(e.target.value)}
+                onFocus={() => setMatError(false)}
+              />
+              <button
+                className="p-2 w-full bg-yellow-400 border border-black text-black"
+                disabled={useMatNum}
+                onClick={async () => {
+                  setUseMatNum(true);
+                  try {
+                    if (!matricNumber) return setMatError(true);
+                    let { data } = await axios.get(
+                      `/api/search?matricNumber=${matricNumber}`
+                    );
+                    if (data.alumini) {
+                      setFoundAlumini(data.alumini);
+                      setStage(2);
+                    }
+                  } catch (error) {
+                    if (!error.response) {
+                      toaster.danger("Unable to check alumini", {
+                        description: "May be a network error",
+                      });
+                    } else if (error.response.status === 500) {
+                      toaster.danger("Unable to check alumini", {
+                        description:
+                          "May be a problem from our side. We'll investigate",
+                      });
+                    }
+                  } finally {
+                    setUseMatNum(false);
+                  }
+                }}
+              >
+                {useMatNum ? "Checking" : "Check"}
+              </button>
+              {matError && <p className="text-red-500">Enter Matric Number</p>}
+            </div>
+          ) : (
+            stage === 2 && (
+              <AluminiForm
+                alumini={foundAlumini}
+                cb={updateAlumini}
+                loading={editingAlumini}
+                setLoading={setEditingAlumini}
+              />
+            )
+          )}
+        </div>
+      </Dialog>
+      <Dialog
+        isShown={showSearchModal}
+        title="Search Alumini"
+        hasFooter={false}
+        // hasHeader={false}
+        shouldCloseOnOverlayClick={true}
+        containerProps={{ backgroundColor: "black" }}
+        preventBodyScrolling
+        onCloseComplete={() => {
+          setShowSearchModal(false);
+          setFoundSearch({});
+        }}
+      >
+        <div
+          id="alumini_form"
+          className="rounded flex flex-col items-center p-6 bg-black text-white phone:p-0"
+        >
+          {/* <span
+            className="absolute right-4 top-1 text-3xl cursor-pointer"
+            onClick={() => {
+              setShowSearchModal(false);
+              setFoundSearch({});
+            }}
+          >
+            &times;
+          </span> */}
+          <div>
+            <input
+              className="p-2  bg-black border border-yellow-400 text-yellow-400 w-full mb-2"
+              placeholder="Matric Number"
+              required
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onFocus={() => setSearchError(false)}
+            />
+            <button
+              className="p-2 w-full bg-yellow-400 border border-black text-black"
+              disabled={searchingAlumini}
+              onClick={async () => {
+                setSearchingAlumini(true);
+                try {
+                  if (!search) return setSearchError(true);
+                  let { data } = await axios.get(
+                    `/api/search?matricNumber=${search}`
+                  );
+                  if (data.alumini) {
+                    setFoundSearch(data.alumini);
+                  }
+                } catch (error) {
+                  if (!error.response) {
+                    toaster.danger("Unable to search for alumini", {
+                      description: "May be a network error",
+                    });
+                  } else if (error.response.status === 500) {
+                    toaster.danger("Unable to search for alumini", {
+                      description:
+                        "May be a problem from our side. We'll investigate",
+                    });
+                  }
+                } finally {
+                  setSearchingAlumini(false);
+                }
+              }}
+            >
+              {searchingAlumini ? "Searching" : "Search"}
+            </button>
+            {searchError && <p className="text-red-500">Enter Matric Number</p>}
+          </div>
+          {searchingAlumini ? (
+            <Spinner className="mt-5" />
+          ) : foundSearch.firstname ? (
+            <div className="rounded flex flex-col items-center p-6 bg-black text-white phone:p-0">
+              <span
+                className="absolute right-4 top-1 text-3xl cursor-pointer"
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                &times;
+              </span>
+              <img
+                src={foundSearch?.image}
+                className="w-32 h-32 rounded-full mb-5"
+              />
+              <div>
+                <p className="text-3xl font-semibold text-center text-yellow-400">
+                  {foundSearch?.firstname + " " + foundSearch?.lastname}
+                </p>
+                <p className="italic mb-4 text-xl text-center">
+                  {foundSearch?.admissionYear} - {foundSearch?.gradYear}
+                </p>
+                <p>
+                  <span className="font-bold mb-2 text-xl">CGPA:</span>{" "}
+                  {foundSearch?.cgpa}
+                </p>
+                <p>
+                  <span className="font-bold mb-2 text-xl">Email:</span>{" "}
+                  {foundSearch?.email}
+                </p>
+                <p>
+                  <span className="font-bold mb-2 text-xl">Phone:</span>{" "}
+                  {foundSearch?.phone}
+                </p>
+                <p>
+                  <span className="font-bold mb-2 text-xl">Interst:</span>{" "}
+                  {foundSearch?.interest}
+                </p>
+                <p>
+                  <span className="font-bold mb-2 text-xl">Best Lecturer:</span>{" "}
+                  {foundSearch?.bestLecturer}
+                </p>
+              </div>
+            </div>
+          ) : (
+            <p className="text-yellow-400 text-xl mt-10">Nothing here...</p>
+          )}
         </div>
       </Dialog>
     </div>
